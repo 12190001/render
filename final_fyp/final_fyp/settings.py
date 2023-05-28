9beac106596c5b49735d95146c6c09cbe122bed8
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-05-28 16:22:22 +0600

    Update settings.py

--- final_fyp/final_fyp/settings.py
+++ final_fyp/final_fyp/settings.py
@@ -84,7 +84,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [(os.environ.get('rediss://red-chpiin67avjb90n1f8u0:96HAJBNLtbRZbhutkcQFxBCTXjjjhymd@singapore-redis.render.com:6379', 'redis://localhost:6379'))],
+            'hosts': [('rediss://red-chpiin67avjb90n1f8u0:96HAJBNLtbRZbhutkcQFxBCTXjjjhymd@singapore-redis.render.com:6379')],
         },
     },
 }
