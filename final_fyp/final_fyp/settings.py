b790ff7aa6b78c45ae33adafbe04caa374979de0
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-05-31 20:23:06 +0600

    Update settings.py

--- final_fyp/final_fyp/settings.py
+++ final_fyp/final_fyp/settings.py
@@ -42,14 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'food'
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
