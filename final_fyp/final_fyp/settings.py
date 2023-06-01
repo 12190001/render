a92be9187820382777c5d4410de5ba02b2c1ca53
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-06-01 23:15:10 +0600

    Update settings.py

--- final_fyp/final_fyp/settings.py
+++ final_fyp/final_fyp/settings.py
@@ -92,25 +92,25 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME':'food-ordering',
-        'CLIENT': {
-            "host":"mongodb+srv://fyp:fyp@cluster0.vhaucyj.mongodb.net/?retryWrites=true&w=majority",
-            'username':'fyp',
-            'password':'fyp'
-        }
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-
+#         'ENGINE': 'djongo',
+#         'NAME':'food-ordering',
+#         'CLIENT': {
+#             "host":"mongodb+srv://fyp:fyp@cluster0.vhaucyj.mongodb.net/?retryWrites=true&w=majority",
+#             'username':'fyp',
+#             'password':'fyp'
+#         }
+#     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+       }
+
+}
 
 
 # Password validation
