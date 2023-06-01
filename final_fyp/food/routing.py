cfdefe98cb35d7bbbf9b85c6f4fe30449f9f2fb8
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-06-01 16:28:07 +0600

    Update routing.py

--- final_fyp/food/routing.py
+++ final_fyp/food/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/notifications/', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'wss://gofoodie-1a86.onrender.com/ws/notifications/', consumers.NotificationConsumer.as_asgi()),
+
 ]
 
