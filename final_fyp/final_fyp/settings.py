5378017c2f414936844110cd7dcf205d2aece327
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-06-01 13:10:57 +0600

    Update settings.py

--- final_fyp/final_fyp/settings.py
+++ final_fyp/final_fyp/settings.py
@@ -35,13 +35,13 @@ ALLOWED_HOSTS = ['gofoodie-1a86.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     'food'
 ]
 CORS_ORIGIN_ALLOW_ALL = True
