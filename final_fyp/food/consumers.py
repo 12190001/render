e127e991b2679e1a98aeb00039d9f786237fe0c2
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-06-01 17:48:18 +0600

    Update consumers.py

--- final_fyp/food/consumers.py
+++ final_fyp/food/consumers.py
@@ -1,17 +1,9 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-import websockets
-import ssl
 
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Create an SSL context
-        ssl_context = ssl.create_default_context()
-
-        # Set up the secure WebSocket connection
-        self.ws = await websockets.connect('wss://gofoodie-1a86.onrender.com/ws/notifications/', ssl=ssl_context)
-
         # Join the "notification" group
         await self.channel_layer.group_add("notification", self.channel_name)
         await self.accept()
@@ -20,18 +12,16 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         # Leave the "notification" group
         await self.channel_layer.group_discard("notification", self.channel_name)
 
-        # Close the WebSocket connection
-        await self.ws.close()
-
     async def notify_manager(self, event):
-        await self.ws.send(json.dumps({
+        # Send a notification to the manager
+        await self.send(text_data=json.dumps({
             "type": "notification",
             "message": event["message"],
         }))
-
     async def notify_customer(self, event):
-        await self.ws.send(json.dumps({
+        # Send a notification to the manager
+        await self.send(text_data=json.dumps({
             "type": "notification",
             "customer": event["customer"],
-            "status": event["status"]
+            "status":event["status"]
         }))
