ef31cddf687aee07082035f9e877b9665ea05a00
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-06-05 08:41:01 +0600

    Update context_processors.py

--- final_fyp/food/context_processors.py
+++ final_fyp/food/context_processors.py
@@ -25,37 +25,37 @@ def notifications(request):
         cust_notifications = ''
     return {'notification':allnotifications[::-1], 'cust_notifications':cust_notifications[::-1]}
 
-def top_food(request):
-    context = {}
-    top_ordered_food = OrderItems.objects.values('menu_id__item_name', 'menu_id__image', 'menu_id__description', 'menu_id__price').annotate(total_ordered=Sum('quantity')).order_by('-total_ordered')[:3]
-    import os
-    from django.conf import settings
+# def top_food(request):
+#     context = {}
+#     top_ordered_food = OrderItems.objects.values('menu_id__item_name', 'menu_id__image', 'menu_id__description', 'menu_id__price').annotate(total_ordered=Sum('quantity')).order_by('-total_ordered')[:3]
+#     import os
+#     from django.conf import settings
 
-    # assume that the string is "/media/my_image.png"
-    for i in top_ordered_food:
-        print(i['menu_id__image'])
-        image_url = os.path.join(settings.MEDIA_URL, i['menu_id__image'])
-        print(image_url)
-        i['menu_id__image'] = image_url
-        menu_item = MenuItems.objects.get(item_name=i['menu_id__item_name'])
-        menu_item.is_top_ordered = True
-        menu_item.save()
+#     # assume that the string is "/media/my_image.png"
+#     for i in top_ordered_food:
+#         print(i['menu_id__image'])
+#         image_url = os.path.join(settings.MEDIA_URL, i['menu_id__image'])
+#         print(image_url)
+#         i['menu_id__image'] = image_url
+#         menu_item = MenuItems.objects.get(item_name=i['menu_id__item_name'])
+#         menu_item.is_top_ordered = True
+#         menu_item.save()
 
-        new_items = MenuItems.objects.order_by('-creation_date')[:2]
+#         new_items = MenuItems.objects.order_by('-creation_date')[:2]
 
-        for item in new_items:
-            item.is_new_item = True
-            item.save()
-        MenuItems.objects.exclude(pk__in=[item.pk for item in new_items]).update(is_new_item=False)
-#         context['newitems'] = 
-#         context['menu'] = 
-#         context['top_ordered_food'] =
-        context = {
-            'newitems':new_items ,
-            'menu':MenuItems.objects.all() ,
-            'top_ordered_food':  top_ordered_food
-        }
-    return context
+#         for item in new_items:
+#             item.is_new_item = True
+#             item.save()
+#         MenuItems.objects.exclude(pk__in=[item.pk for item in new_items]).update(is_new_item=False)
+# #         context['newitems'] = 
+# #         context['menu'] = 
+# #         context['top_ordered_food'] =
+#         context = {
+#             'newitems':new_items ,
+#             'menu':MenuItems.objects.all() ,
+#             'top_ordered_food':  top_ordered_food
+#         }
+#     return context
 
 
 
