7cd65c9a69fea08a698077143daf98252673c731
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-06-09 13:46:53 +0600

    Update views.py

--- final_fyp/food/views.py
+++ final_fyp/food/views.py
@@ -362,11 +362,12 @@ def owner_profile(request):
                 last_name = request.POST['last_name'] if request.POST['last_name'] != "" else profile.last_name
                 email = request.POST['email'] if request.POST['email'] != "" else profile.email
                 contact_number = request.POST['contact'] if request.POST['contact'] != "" else profile.contact_number
-                filename = fs.save(image.name, image)
-                file_path = os.path.join(settings.MEDIA_ROOT, filename)
+                
                 fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+                filename = fs.save(image.name, image)
+                
 #                 profile.save()
-                CustomUser.objects.filter(email = request.user).update(image = image, first_name=first_name,last_name=last_name,email=email,contact_number=contact_number)
+                CustomUser.objects.filter(email = request.user).update(image = fs.url(filename), first_name=first_name,last_name=last_name,email=email,contact_number=contact_number)
                 messages.success(request, 'Your profile has been updated.')
                 return redirect('owner_profile')
     return render(request, 'owner_final/owner_profile.html')
